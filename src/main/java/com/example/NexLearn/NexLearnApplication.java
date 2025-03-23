c79655254842ad4464c880f27c566601549c781a
Gawrangana <gawranganaKarunarathna@gmail.com>
2025-03-23 15:46:27 +0530

    new commit

---
+++ src/main/java/com/example/NexLearn/NexLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.NexLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexLearnApplication.class, args);
+	}
+
+}
